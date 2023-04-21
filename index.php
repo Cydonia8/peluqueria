d6ec0cb2bd1827e0aa02f575adbdce96cde9633c
Cydonia8 <holymustaine20@gmail.com>
2023-04-21 12:41:37 +0200

    forms

--- index.php
+++ index.php
@@ -13,6 +13,37 @@
 <body>
     <section class="formularios">
         <form data-form="login" action="#" method="post">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Correo</label>
+                <input type="email" class="form-control" name="correo" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                <input type="password" name="pass" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+        </form>
+        <form data-form="register" action="#" method="post">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="nombre" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Correo</label>
+                <input type="email" name="correo" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                <input type="password" name="pass" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Teléfono</label>
+                <input type="tel" pattern="[6-7]{1}[0-9]{8}" name="telefono" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Registrarme</button>
+        </form>
+        <!-- <form data-form="login" action="#" method="post">
             <input type="password" name="pass" required>
             <input type="email" name="correo" placeholder="Correo">
             <input type="submit" value="Iniciar sesión">
@@ -23,7 +54,7 @@
             <input type="password" name="pass" required>
             <input type="tel" name="telefono" pattern="[6-7]{1}[0-9]{8}" required>
             <input type="submit" value="Registrarme">
-        </form>
+        </form> -->
     </section>
     <section class="buttons">
         <button class="login-register-buttons" data-button="login">Iniciar sesión</button>
