3a1f75228efcf41c9367d148a97d8ecf85d3dece
Cydonia8 <holymustaine20@gmail.com>
2023-04-25 11:24:45 +0200

    estilos

--- php/calendario.php
+++ php/calendario.php
@@ -31,7 +31,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="../estilos.css">
     <script src="../scripts/servicios.js" defer></script>
     <title>Bienvenido</title>
 </head>
@@ -96,7 +96,7 @@
         </div>
     </div>
 
-    <section>
+    <section class="container-fluid px-sm-3 px-0 mt-4 row">
     <?php
         if(!isset($_SESSION['user'])){
             header('Refresh: 0; URL=../index.php');
@@ -160,7 +160,7 @@
                 $anio=$año_anterior;
             }
 
-            echo "<table>
+            echo "<table id=\"calendario\">
                 <caption>
                     <a href='calendario.php?mes=$mes_anterior&año=$año_anterior'><-</a>
                     $nombre_mes
