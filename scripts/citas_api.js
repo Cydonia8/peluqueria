84d5680e4ab72bcc9aa32044eb6659fbe867bdf9
Cydonia8 <holymustaine20@gmail.com>
2023-04-26 13:41:39 +0200

    filtrar horas

--- scripts/citas_api.js
+++ scripts/citas_api.js
@@ -41,30 +41,6 @@ select_horas.addEventListener("click", ()=>{
     }
 })
 
-// select_horas.addEventListener("click", ()=>{
-//     let filtrado=lista.filter(cit=>cit.fecha===select_fecha.value);
-//     if(select_trabajador.value!='Elige con quien quieres la cita'){
-//         filtrado=filtrado.filter(cit=>cit.id_trab===select_trabajador.value);
-//     }
-//     filtrado.forEach(opcion=>{
-//         let dur=opcion.duracion.split(":");
-//         let time=opcion.hora.split(":");
-//         let ocupado_min=dur[1]+time[1];
-//         let ocupado_h=dur[0]+time[0];
-//         if(ocupado_min>60){
-//             ocupado_h+=ocupado_min/60;
-//             ocupado_min%=60;
-//         }
-//         let fin=ocupado_h+":"+ocupado_min;
-//         select_horas.children.forEach(child=>{
-//             if(!child.innerText>=opcion.duracion && child.innerText<=fin){
-//                 child.style.display="none";
-//             }
-//         })
-//     })
-//     console.log(filtrado);
-// })
-
 
 
 
