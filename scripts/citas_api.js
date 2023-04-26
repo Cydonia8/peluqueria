8cdd3e2e9528e466577042bacb6037d65fa4788a
Daniel <daniel.gomez.garcia3300@gmail.com>
2023-04-26 13:09:23 +0200

    pruebas

--- scripts/citas_api.js
+++ scripts/citas_api.js
@@ -14,6 +14,7 @@ async function datos(){
 }
 
 select_horas.addEventListener("click", ()=>{
+    console.log(select_fecha.value);
     if(select_fecha.value != ''){
         let citas_trab = lista.filter(cita=>cita.id_trab===select_trabajador.value && cita.fecha === select_fecha.value)
         console.log(citas_trab)
@@ -21,6 +22,23 @@ select_horas.addEventListener("click", ()=>{
         const option = createOption('', "Elige trabajador y fecha")
         select_horas.innerHTML=''
         select_horas.appendChild(option)
+        
+        filtrado.forEach(opcion=>{
+            let dur=opcion.duracion.split(":");
+            let time=opcion.hora.split(":");
+            let ocupado_min=dur[1]+time[1];
+            let ocupado_h=dur[0]+time[0];
+            if(ocupado_min>60){
+                ocupado_h+=ocupado_min/60;
+                ocupado_min%=60;
+            }
+            let fin=ocupado_h+":"+ocupado_min;
+            select_horas.children.forEach(child=>{
+                if(child.innerText>=opcion.duracion && child.innerText<=fin){
+                    child.style.display="none";
+                }
+            })
+        })
     }
 })
 
