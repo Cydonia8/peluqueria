035645c7ecfae15efd1b96bf503734079bd1f7b3
Daniel <daniel.gomez.garcia3300@gmail.com>
2023-04-28 13:57:04 +0200

    ajuste bd

--- peluqueria.sql
+++ peluqueria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-04-2023 a las 11:39:11
+-- Tiempo de generación: 28-04-2023 a las 13:55:55
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -35,6 +35,16 @@ CREATE TABLE `citas` (
   `servicio` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `citas`
+--
+
+INSERT INTO `citas` (`cliente`, `trabajador`, `fecha`, `hora`, `servicio`) VALUES
+(1, 3, '2023-04-28', '11:30:00', 1),
+(1, 3, '2023-04-29', '09:00:00', 1),
+(1, 6, '2023-04-27', '09:00:00', 1),
+(4, 3, '2023-04-28', '18:00:00', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -42,18 +52,20 @@ CREATE TABLE `citas` (
 --
 
 CREATE TABLE `horario` (
+  `id` bigint(20) NOT NULL,
   `m_apertura` time NOT NULL,
   `m_cierre` time NOT NULL,
   `t_apertura` time NOT NULL,
-  `t_cierre` time NOT NULL
+  `t_cierre` time NOT NULL,
+  `dia` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `horario`
 --
 
-INSERT INTO `horario` (`m_apertura`, `m_cierre`, `t_apertura`, `t_cierre`) VALUES
-('13:08:00', '13:20:00', '13:25:00', '13:30:00');
+INSERT INTO `horario` (`id`, `m_apertura`, `m_cierre`, `t_apertura`, `t_cierre`, `dia`) VALUES
+(1, '09:00:00', '14:00:00', '17:30:00', '20:30:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -62,23 +74,39 @@ INSERT INTO `horario` (`m_apertura`, `m_cierre`, `t_apertura`, `t_cierre`) VALUE
 --
 
 CREATE TABLE `personas` (
-  `id` bigint(1) NOT NULL,
+  `id` bigint(20) NOT NULL,
   `nombre` varchar(75) NOT NULL,
   `correo` varchar(75) NOT NULL,
   `telefono` char(9) NOT NULL,
   `tipo` bigint(20) NOT NULL,
   `pass` varchar(20) NOT NULL,
-  `activo` tinyint(4) NOT NULL DEFAULT 1
+  `activo` tinyint(4) NOT NULL DEFAULT 1,
+  `f_inicio` date DEFAULT NULL,
+  `f_fin` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `personas`
 --
 
-INSERT INTO `personas` (`id`, `nombre`, `correo`, `telefono`, `tipo`, `pass`, `activo`) VALUES
-(1, 'admin', 'admin', '', 3, '', 1),
-(2, 'cliente', '', '', 1, '', 1),
-(3, 'trabajador', '', '', 2, '', 1);
+INSERT INTO `personas` (`id`, `nombre`, `correo`, `telefono`, `tipo`, `pass`, `activo`, `f_inicio`, `f_fin`) VALUES
+(1, 'admin', 'admin@admin.com', '', 3, 'admin', 1, NULL, NULL),
+(2, 'cliente', '', '', 1, '', 1, NULL, NULL),
+(3, 'trabajador', 'traba@gmail.com', '', 2, '', 1, NULL, NULL),
+(4, 'Pepe', 'pepe@gmail.com', '123123123', 1, 'pepe', 1, NULL, NULL),
+(5, 'juan', 'juan@gmail.com', '111111111', 1, 'juan', 1, NULL, NULL),
+(6, 'maria', 'maria@gmail.com', '', 2, 'maria', 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `realiza`
+--
+
+CREATE TABLE `realiza` (
+  `empleado` bigint(20) NOT NULL,
+  `servicio` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -90,7 +118,7 @@ CREATE TABLE `servicios` (
   `id` bigint(20) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `duracion` time NOT NULL,
-  `precio` float(5,2) NOT NULL,
+  `precio` float(5,2) DEFAULT NULL,
   `activo` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -121,6 +149,20 @@ INSERT INTO `tipos` (`id`, `nombre`) VALUES
 (2, 'Trabajador'),
 (3, 'Administrador');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `trabaja`
+--
+
+CREATE TABLE `trabaja` (
+  `empleado` bigint(20) NOT NULL,
+  `m_inicio` time DEFAULT NULL,
+  `m_fin` time DEFAULT NULL,
+  `t_inicio` time DEFAULT NULL,
+  `t-fin` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tablas volcadas
 --
@@ -130,14 +172,14 @@ INSERT INTO `tipos` (`id`, `nombre`) VALUES
 --
 ALTER TABLE `citas`
   ADD PRIMARY KEY (`cliente`,`trabajador`,`fecha`,`hora`),
-  ADD KEY `fk_cit_trabajador` (`trabajador`),
-  ADD KEY `fk_cit_servicio` (`servicio`);
+  ADD KEY `fk_cit_servicio` (`servicio`),
+  ADD KEY `fk_cit_trabajador` (`trabajador`);
 
 --
 -- Indices de la tabla `horario`
 --
 ALTER TABLE `horario`
-  ADD PRIMARY KEY (`m_apertura`,`m_cierre`,`t_apertura`,`t_cierre`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `personas`
@@ -145,6 +187,14 @@ ALTER TABLE `horario`
 ALTER TABLE `personas`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_per_tipo` (`tipo`);
+
+--
+-- Indices de la tabla `realiza`
+--
+ALTER TABLE `realiza`
+  ADD PRIMARY KEY (`empleado`,`servicio`),
+  ADD KEY `ce_rea_pers` (`empleado`),
+  ADD KEY `ce_rea_serv` (`servicio`);
 
 --
 -- Indices de la tabla `servicios`
@@ -159,14 +209,26 @@ ALTER TABLE `tipos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `trabaja`
+--
+ALTER TABLE `trabaja`
+  ADD PRIMARY KEY (`empleado`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `horario`
+--
+ALTER TABLE `horario`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `id` bigint(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
@@ -197,6 +259,19 @@ ALTER TABLE `citas`
 --
 ALTER TABLE `personas`
   ADD CONSTRAINT `fk_per_tipo` FOREIGN KEY (`tipo`) REFERENCES `tipos` (`id`);
+
+--
+-- Filtros para la tabla `realiza`
+--
+ALTER TABLE `realiza`
+  ADD CONSTRAINT `ce_rea_pers` FOREIGN KEY (`empleado`) REFERENCES `personas` (`id`),
+  ADD CONSTRAINT `ce_rea_serv` FOREIGN KEY (`servicio`) REFERENCES `servicios` (`id`);
+
+--
+-- Filtros para la tabla `trabaja`
+--
+ALTER TABLE `trabaja`
+  ADD CONSTRAINT `fk_tra_per` FOREIGN KEY (`empleado`) REFERENCES `personas` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
