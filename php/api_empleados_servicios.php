02e955e94772d8d8cbc1aba3a10aaa8db1feb583
Daniel <daniel.gomez.garcia3300@gmail.com>
2023-05-08 11:50:36 +0200

    guardar

--- php/api_empleados_servicios.php
+++ php/api_empleados_servicios.php
@@ -5,7 +5,7 @@
     
     $conexion = new mysqli('localhost', 'root', '', 'peluqueria');
     sleep(1);
-    $sentencia = $conexion->query("select nombre, id from personas p, realiza r where p.id = r.empleado and r.servicio ");
+    $sentencia = $conexion->query("select nombre, id, f_inicio, f_fin, m_inicio, m_fin, t_inicio, t_fin ,fecha ,hora ,servicio ,duracion from personas p, realiza r, trabaja t, citas c, servicios s where s.id=c.servicio and p.id=trabajador and p.id=t.empleado and p.id = r.empleado and r.servicio=?");
     $datos = [];
     
     while($fila = $sentencia->fetch_array(MYSQLI_ASSOC)){
