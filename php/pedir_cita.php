d0faa7b1dbd5880490c2491557befaacd7616024
Daniel <daniel.gomez.garcia3300@gmail.com>
2023-05-10 11:11:01 +0200

    prueba dias

--- php/pedir_cita.php
+++ php/pedir_cita.php
@@ -18,7 +18,7 @@
     <link href="../vanilla-calendar-main/vanilla-calendar-main/package/build/themes/dark.min.css" rel="stylesheet">
     <link href="../vanilla-calendar-main/vanilla-calendar-main/package/build/themes/light.min.css" rel="stylesheet">
     <!-- Plugin JS -->
-    <script src="../vanilla-calendar-main/vanilla-calendar-main/package/build/vanilla-calendar.min.js" defer></script>
+    <script src="../vanilla-calendar-main/vanilla-calendar-main/package/build/vanilla-calendar.min.js" ></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <title>Bienvenido</title>
--- scripts/pedir_cita_api.js
+++ scripts/pedir_cita_api.js
@@ -4,7 +4,7 @@ const calendario = document.querySelectorAll("#calendario td:not(:empty)")
 const flechas = document.querySelectorAll("caption a")
 const select_servicio = document.getElementById("select-servicio")
 const select_trabajador = document.getElementById("select-empleado")
-let fiestas
+// let fiestas
 
 let fechas_horas
 horarios();
@@ -15,24 +15,6 @@ async function horarios(){
     fechas_horas=datos["datos"]
 }
 
-festivos()
-async function festivos(){
-    const respuesta = await fetch('https://date.nager.at/api/v3/PublicHolidays/2023/ES')
-    const datos = await respuesta.json()
-    fiestas = datos.filter(festivo => festivo.counties == null || festivo.counties.includes("ES-AN"))
-    calendario.forEach(cal=>{
-        let fecha = cal.getAttribute("data-date")
-        fiestas.forEach(festivo=>{
-            if(festivo.date == fecha){
-                cal.classList.add("festivo")
-            }
-        })
-    })
-}
-flechas.forEach(flecha=>{
-    flecha.addEventListener("click", comprobarFestivos)
-})
-
 select_servicio.addEventListener("change", async ()=>{
     const servicio = select_servicio.value
     const res = await fetch(`../php/api_empleados_servicios.php?id=${servicio}`)
@@ -40,17 +22,6 @@ select_servicio.addEventListener("change", async ()=>{
     const info = datos["datos"]
     selectTrabajador(info, servicio)
 })
-
-function comprobarFestivos(){
-    calendario.forEach(cal=>{
-        let fecha = cal.getAttribute("data-date")
-        fiestas.forEach(festivo=>{
-            if(festivo.date == fecha){
-                cal.classList.add("festivo")
-            }
-        })
-    })
-}
 
 function selectTrabajador(info, servicio){
     select_trabajador.innerHTML=''
@@ -66,12 +37,12 @@ function selectTrabajador(info, servicio){
         select_trabajador.appendChild(opt)
     })
 }
-
+let fiestas;
 document.addEventListener('DOMContentLoaded',async () => {
     const respuesta = await fetch('https://date.nager.at/api/v3/PublicHolidays/2023/ES')
     const datos = await respuesta.json()
-    let fiestas = datos.filter(festivo => festivo.counties == null || festivo.counties.includes("ES-AN")).map(f=>f.date);
-
+    fiestas = datos.filter(festivo => festivo.counties == null || festivo.counties.includes("ES-AN")).map(f=>f.date);
+ 
     const calendar = new VanillaCalendar('#calendar',{
         settings: {
             range: {
@@ -81,29 +52,22 @@ document.addEventListener('DOMContentLoaded',async () => {
             selected: {
                 holidays: fiestas,
             },
-          },
+        },
     });
     calendar.settings.lang = 'es';
     calendar.init();
-
-
-    const botones=document.getElementById("calendar").querySelectorAll(".vanilla-calendar-days>div>button")
+    
     const dias=document.getElementById("calendar").querySelectorAll(".vanilla-calendar-days>div")
-    dias.forEach(dia=>dia.addEventListener("click",()=>{
-        console.log("a")
-        formatear_calendario();
-    }))
-    // let descansos=fechas_horas.descanso.map(d=>d.dia);
-    formatear_calendario();
-    function formatear_calendario(){
-        const dias=document.getElementById("calendar").querySelectorAll(".vanilla-calendar-days>div")
-        fechas_horas.descanso.forEach(d=>{
-            
-            for(let i=d.dia-1;i<dias.length;i+=7){
-                dias[i].classList.add("prueba")
-                dias[i].children[0].setAttribute("disabled",true)
-            }
-            
-        })
-    }
+    fechas_horas.descanso.forEach(d=>{   
+        for(let i=d.dia-1;i<dias.length;i+=7){
+            dias[i].children[0].classList.add("vanilla-calendar-day__btn_holiday")
+        }
+        
+    dias.forEach(dia=>{
+        dia.children[0].setAttribute("disabled",true)
+    })
+        
+    })
 });
+
+
