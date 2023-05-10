c13cd7320ae2bb3a5d83044b75818fb8bcdf64e5
Daniel <daniel.gomez.garcia3300@gmail.com>
2023-05-10 11:30:18 +0200

    raro

--- scripts/pedir_cita_api.js
+++ scripts/pedir_cita_api.js
@@ -62,7 +62,7 @@ select_servicio.addEventListener("change", async ()=>{
     selectTrabajador(info)
 })
 
-function selectTrabajador(info, servicio){
+function selectTrabajador(info){
     select_trabajador.innerHTML=''
     if(info["realiza"].length > 1){
         select_trabajador.innerHTML='<option checked hidden>Elige un trabajador</option>'
@@ -76,37 +76,3 @@ function selectTrabajador(info, servicio){
         select_trabajador.appendChild(opt)
     })
 }
-
-// let fiestas;
-
-// document.addEventListener('DOMContentLoaded',async () => {
-//     const respuesta = await fetch('https://date.nager.at/api/v3/PublicHolidays/2023/ES')
-//     const datos = await respuesta.json()
-//     fiestas = datos.filter(festivo => festivo.counties == null || festivo.counties.includes("ES-AN")).map(f=>f.date);
- 
-//     const calendar = new VanillaCalendar('#calendar',{
-//         settings: {
-//             range: {
-//                 disabled: [], // disabled dates
-//                 enabled: [], // disabled dates
-//             },
-//             selected: {
-//                 holidays: fiestas,
-//             },
-//         },
-//     });
-//     calendar.settings.lang = 'es';
-//     calendar.init();
-    
-//     const dias=document.getElementById("calendar").querySelectorAll(".vanilla-calendar-days>div")
-//     fechas_horas.descanso.forEach(d=>{   
-//         for(let i=d.dia-1;i<dias.length;i+=7){
-//             dias[i].children[0].classList.add("vanilla-calendar-day__btn_holiday")
-//         }
-//     })
-        
-//     dias.forEach(dia=>{
-//         dia.children[0].setAttribute("disabled",true)
-//     })
-        
-// });
